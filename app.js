083b774031ac76e77d7d565d78947579fe75b6a0
Vikram Bawane <vikram@vaminfosys.com>
2024-09-20 00:56:35 +0530

    demo2

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');       // Send a simple response
 });
 
+//do coding
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
