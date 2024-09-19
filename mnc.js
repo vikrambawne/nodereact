3794be1fc7b5d58e7e1fe66ea1b32bd2c5568b43
Vikram Bawane <vikram@vaminfosys.com>
2024-09-20 01:51:18 +0530

    squashed commit of the following:
    commited file
    
    commit a15c0d3ed95d457dab9e7f234692456f2b92f40a
    Author: Vikram Bawane <vikram@vaminfosys.com>
    Date:   Fri Sep 20 01:49:21 2024 +0530
    
        world cool
    
    commit da70e1161945bd326988c82f3fb6bd755e61bf4a
    Author: Vikram Bawane <vikram@vaminfosys.com>
    Date:   Fri Sep 20 01:47:26 2024 +0530
    
        pqr

--- mnc.js
+++ mnc.js
@@ -1,14 +1,7 @@
-// app.js
-const express = require('express'); // Import express
-const app = express();              // Create an express app
+hello AudioWorkletNode
 
-// Define a route for the root URL
-app.get('/', (req, res) => {
-    res.send('Hello, World!');       // Send a simple response
-});
+auditoriam
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+
+opesnns
+dkdkd
